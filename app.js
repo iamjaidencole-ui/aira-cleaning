44a79541b35c7c7b21335d0fbff99fdb2d036b18
iamjaidencole-ui <iamjaidencole@gmail.com>
2025-12-29 17:44:01 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("nav-sticky");
+    } else {
+        navbar.classList.remove("nav-sticky");
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+});
+
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+// home-5
+
+var swiper = new Swiper(".homeSwiper", {
+    direction: "vertical",
+    mousewheel: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
